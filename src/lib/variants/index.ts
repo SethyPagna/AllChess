fc422e78be258ae04300d6aff72928d4dade1da9
SethyPagna <ungsethypagna30@gmail.com>
2026-05-13 02:31:02 +0800

    add src/lib/variants/index.ts

---
+++ src/lib/variants/index.ts
@@ -0,0 +1,3 @@
+export { variantCatalog, getVariant, type VariantKey } from "./catalog";
+export { applyMove, createInitialState, getLegalMoves, serializeSquare, sameSquare } from "./engine";
+export type { BoardCell, GameState, Move, Piece, PlayerClock, Square, VariantDefinition } from "./types";
